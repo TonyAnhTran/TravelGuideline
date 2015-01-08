2da0e44419ea936fb7e14d10f82ae07fc4c24fc8
Tu (Tony) A. TRAN <tu.tran@enclave.vn>
2015-01-08 10:33:29 +0700

    Update folder struture.

---
+++ Travel Guides/Helper/DatabaseHelper.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseHelper.h
+//  Travel Guides
+//
+//  Created by Tu (Tony) A. TRAN on 1/8/15.
+//  Copyright (c) 2015 Tu (Tony) A. TRAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseHelper : NSObject
+
+@end
